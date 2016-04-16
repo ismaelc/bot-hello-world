61301d8a40901a25beb9e10352b63c6c162366b5
Chris Ismael <chris.ismael@gmail.com>
2016-04-15 17:04:35 -0700

    try next()

--- app.js
+++ app.js
@@ -7,53 +7,47 @@ var server = restify.createServer();
 var BOT_ID = process.env.BOT_APP_ID;
 var PRIMARY_SECRET = process.env.BOT_PRIMARY_SECRET;
 
-var helloBot = new builder.BotConnectorBot({ appId: BOT_ID, appSecret: PRIMARY_SECRET });
+var helloBot = new builder.BotConnectorBot({
+    appId: BOT_ID,
+    appSecret: PRIMARY_SECRET
+});
 helloBot.add('/', new builder.CommandDialog()
     .matches('^set name', builder.DialogAction.beginDialog('/profile'))
-    //.matches('search box', builder.DialogAction.beginDialog('/search_box'))
+    .matches('call api', builder.DialogAction.beginDialog('/call-api'))
     .matches('^quit', builder.DialogAction.endDialog())
-    .onDefault(function (session) {
+    .onDefault(function(session) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
         } else {
             session.send('Hello %s!', session.userData.name);
         }
     }));
-/*
-helloBot.add('/search_box', [
-    function (session) {
 
-        //if (session.userData.name) {
+helloBot.add('/call-api', [
 
-            builder.Prompts.text(session, 'What would you like to search?');
-
-        //} else {
-        //    builder.Prompts.text(session, 'Hi! What is your name?');
-        //}
-
-    },
-    function (session, results) {
-        session.userData.query = results.response;
-        session.send('Got it! Searching ' + session.userData.query + '...');
+    function(session, args, next) {
+        //session.userData.query = results.response;
+        session.send('Calling API.. ' + session.userData.query + '...');
 
         var options = {
-          url: 'https://api.github.com/repos/request/request',
-          headers: {
-            'User-Agent': 'request'
-          }
+            url: 'https://api.github.com/repos/request/request',
+            headers: {
+                'User-Agent': 'request'
+            }
         };
 
         function callback(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
-            console.log(info.stargazers_count + " Stars");
-            console.log(info.forks_count + " Forks");
-            session.send(info.stargazers_count + " Stars");
-            //session.endDialog();
-          }
-          else {
-            session.send("Error");
-          }
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                console.log(info.stargazers_count + " Stars");
+                console.log(info.forks_count + " Forks");
+                session.send(info.stargazers_count + " Stars");
+                //session.endDialog();
+            } else {
+                session.send("Error");
+            }
+            
+            next({ response: info });
         }
 
         request(options, callback);
@@ -64,17 +58,16 @@ helloBot.add('/search_box', [
         session.endDialog();
     }
 ]);
-*/
 
-helloBot.add('/profile',  [
-    function (session) {
+helloBot.add('/profile', [
+    function(session) {
         if (session.userData.name) {
             builder.Prompts.text(session, 'What would you like to change it to?');
         } else {
             builder.Prompts.text(session, 'Hi! What is your name?');
         }
     },
-    function (session, results) {
+    function(session, results) {
         session.userData.name = results.response;
         //builder.Prompts.text(session, 'Got it! You are now ' + session.userData.name);
         session.send('Got it! You are now ' + session.userData.name);
@@ -82,11 +75,14 @@ helloBot.add('/profile',  [
     }
 ]);
 
-server.use(helloBot.verifyBotFramework({ appId: BOT_ID, appSecret: PRIMARY_SECRET }));
+server.use(helloBot.verifyBotFramework({
+    appId: BOT_ID,
+    appSecret: PRIMARY_SECRET
+}));
 //server.use(helloBot.verifyBotFramework());
 server.post('/v1/messages', helloBot.listen());
 //server.post('/v1/messages', helloBot.verifyBotFramework(), helloBot.listen());
 
-server.listen(process.env.port || 8080, function () {
+server.listen(process.env.port || 8080, function() {
     console.log('%s listening to %s', server.name, server.url);
 });

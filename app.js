6bfdcbf61d17ecaad9720f6eb54a4b8fa3ef1555
Chris Ismael <chris.ismael@gmail.com>
2016-04-17 11:12:24 -0700

    'test

--- app.js
+++ app.js
@@ -25,59 +25,65 @@ helloBot.add('/', new builder.CommandDialog()
 
 helloBot.add('/call-api', [
 
+    // Need this if we are callini next within this function
     function(session, args, next) {
-    //function(session) {
+        //function(session) {
 
-        //session.userData.query = results.response;
-        //session.send('Calling API.. ' + session.userData.query + '...');
+        if (!session.userDialog.api_response) {
+            //session.userData.query = results.response;
+            //session.send('Calling API.. ' + session.userData.query + '...');
 
-        var options = {
-            url: 'https://api.box.com/2.0/search?query=mp3',
-            headers: {
-                'Authorization': 'Bearer ' + process.env.BOX_API_KEY
-            }
-        };
+            var options = {
+                url: 'https://api.box.com/2.0/search?query=mp3',
+                headers: {
+                    'Authorization': 'Bearer ' + process.env.BOX_API_KEY
+                }
+            };
 
-        function callback(error, response, body) {
-            var result = {};
-            if (!error && response.statusCode == 200) {
-                result = JSON.parse(body);
-                //console.log(result.stargazers_count + " Stars");
-                console.log('Response from Box: ' + JSON.stringify(result));
-                //session.send(result.stargazers_count + " Stars");
+            function callback(error, response, body) {
+                var result = {};
+                if (!error && response.statusCode == 200) {
+                    result = JSON.parse(body);
+                    //console.log(result.stargazers_count + " Stars");
+                    console.log('Response from Box: ' + JSON.stringify(result));
+                    //session.send(result.stargazers_count + " Stars");
+                    //session.endDialog();
+                } else {
+                    console.log('Error: ' + JSON.stringify(error) + "Response: " + JSON.stringify(response));
+                }
+
+
+                next({
+                    response: result
+                });
+
+                //helloBot.beginDialog('/api-response');
                 //session.endDialog();
-            } else {
-                console.log('Error: ' + JSON.stringify(error) + "Response: " + JSON.stringify(response));
             }
 
+            request(options, callback);
 
-            next({
-                response: result
-            });
-
-            //helloBot.beginDialog('/api-response');
-            //session.endDialog();
+            /*
+            console.log('Entered dialog');
+            //session.endDialog('End dialog');
+            builder.DialogAction.endDialog('end dialog');
+            */
         }
-
-        request(options, callback);
-
-        /*
-        console.log('Entered dialog');
-        //session.endDialog('End dialog');
-        builder.DialogAction.endDialog('end dialog');
-        */
+        else {
+            console.log("Went to endDialog on first waterfall..")
+            session.endDialog();
+        }
     }
-    
     ,
     function(session, results) {
         console.log("Gets here");
-        session.userData.api_response = results.response;
+        session.dialogData.api_response = results.response;
         //session.send(JSON.stringify(results));
         //session.endDialog("Got here: " + JSON.stringify(results.response));
         session.endDialog();
     }
 
-    
+
 ]);
 
 /*
